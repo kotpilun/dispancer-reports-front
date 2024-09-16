93ed1be2c13e5c321d96aa091fbe4122d25a1c5e
Oleg Kruglyakov <kotpilun3m@gmail.com>
2024-09-16 11:58:08 +0300

    fix ip

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,21 @@
 export const BFF_CONFIG = {
     HOST: 'localhost',
     PORT: 4444,
-  };
-  
-  // Проверяем доступность удаленного хоста через HTTP-запрос
-  async function checkNetwork() {
+};
+
+export const CATEGORIES = {
+    br: "б/р",
+    thirdU: "3 юн. р.",
+    secondU: "2 юн. р.",
+    firstU: "1 юн. р.",
+    thirdV: "3 р.",
+    secondV: "2 р.",
+    firstV: "1 р.",
+    kms: "КМС"
+};
+
+// Проверяем доступность удаленного хоста через HTTP-запрос
+async function checkNetwork() {
     try {
       const response = await fetch('http://192.168.0.102:4444', { method: 'HEAD' });
       if (response.ok) {
